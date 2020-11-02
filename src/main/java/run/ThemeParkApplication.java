70ae778649ee4c438d13fee2111ac07fbdf77069
avsimvadim <avsimvadim@gmail.com>
2020-11-02 17:17:27 -0500

    first

---
+++ src/main/java/run/ThemeParkApplication.java
@@ -0,0 +1,24 @@
+package run;
+
+import run.entity.ThemeParkRide;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import run.repository.ThemeParkRideRepository;
+
+@SpringBootApplication
+public class ThemeParkApplication  {
+    public static void main(String[] args) {
+        SpringApplication.run(ThemeParkApplication.class);
+    }
+
+    @Bean
+    public CommandLineRunner sampleData(ThemeParkRideRepository repository) {
+        return (args) -> {
+            repository.save(new ThemeParkRide("Rollercoaster", "Train ride that speeds you along.", 5, 3));
+            repository.save(new ThemeParkRide("Log flume", "Boat ride with plenty of splashes.", 3, 2));
+            repository.save(new ThemeParkRide("Teacups", "Spinning ride in a giant tea-cup.", 2, 4));
+        };
+    }
+}
